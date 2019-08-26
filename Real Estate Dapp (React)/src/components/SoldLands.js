f91a031317603954a5ffad64ea3e4b4caad50b70
Chad <49378047+chadracelis@users.noreply.github.com>
2019-08-26 18:48:02 -0400

    Destructuring & fixed console errors

--- Real Estate Dapp (React)/src/components/SoldLands.js
+++ Real Estate Dapp (React)/src/components/SoldLands.js
@@ -1,6 +1,39 @@
 import React, { Component } from 'react'
 
-export class SoldLands extends Component {
+const showSoldLands = (lands, listLand, account, handleChange, value) => {
+  const soldLands = lands.filter(land => land.forSale === false)
+  return (
+    soldLands.map((land) => {
+      return (
+        <tr key={land.landID}>
+          <th scope="row">{land.landID.toString()}</th>
+          <td>{land.location}</td>
+          <td>{window.web3.utils.fromWei(land.value.toString(), 'Ether')} Eth</td>
+          <td>{land.owner}</td>
+          { land.owner === account ? 
+            <td>
+              <input
+                id="value"
+                onChange={handleChange}
+                type="text"
+                className="form-control"
+                placeholder="Listing Price.."
+                required />
+              <button 
+                value={value}
+                className="buyButton"
+                onClick={ (e) => { listLand(land.landID, window.web3.utils.toWei(e.target.value.toString(), 'Ether')) } } 
+              > List Your Land
+              </button>
+            </td> : <td></td>
+          } 
+        </tr>
+      )
+    })
+  )
+}
+
+class SoldLands extends Component {
 
   state = {
     value: null
@@ -13,6 +46,7 @@ export class SoldLands extends Component {
   }
 
   render() {
+    const { lands, listLand, account } = this.props;
     return (
       <div>
         <h2>Sold Lands</h2>
@@ -20,43 +54,14 @@ export class SoldLands extends Component {
           <thead>
             <tr>
               <th scope="col">#</th>
-              <th scope="col">location</th>
-              <th scope="col">value</th>
+              <th scope="col">Location</th>
+              <th scope="col">Value</th>
               <th scope="col">Owner</th>
               <th scope="col"></th>
             </tr>
           </thead>
           <tbody id="landList">
-            { this.props.lands.map((land) => {
-                if(!land.forSale) {
-                  return(
-                    <tr key={land.landID}>
-                      <th scope="row">{land.landID.toString()}</th>
-                      <td>{land.location}</td>
-                      <td>{window.web3.utils.fromWei(land.value.toString(), 'Ether')} Eth</td>
-                      <td>{land.owner}</td>
-                      { land.owner === this.props.account ? 
-                        <td>
-                          <input
-                            id="value"
-                            onChange={this.handleChange}
-                            type="text"
-                            className="form-control"
-                            placeholder="Listing Price.."
-                            required />
-                          <button 
-                            value={this.state.value}
-                            className="buyButton"
-                            onClick={ (e) => {this.props.listLand(land.landID, window.web3.utils.toWei(e.target.value.toString(), 'Ether'))} } 
-                          > List Your Land
-                          </button>
-                        </td> : <div></div>
-                      } 
-                    </tr>
-                  )
-                } 
-              }
-            )}
+            { showSoldLands(lands, listLand, account, this.handleChange, this.state.value) }
           </tbody>
         </table>
       </div>
@@ -65,3 +70,4 @@ export class SoldLands extends Component {
 }
 
 export default SoldLands
+
