9d1d59e34dec90683eae83513aa2196d29291939
Chad <49378047+chadracelis@users.noreply.github.com>
2019-09-18 11:58:58 -0400

    Align form container

--- src/components/AddLand.js
+++ src/components/AddLand.js
@@ -15,7 +15,7 @@ class AddLand extends Component {
   captureFile = (e) => {
     e.preventDefault()
     const file = e.target.files[0]
-    const reader = new window.FileReader() // convert file to a buffer (allows us to sore into ipfs)
+    const reader = new window.FileReader() 
     reader.readAsArrayBuffer(file)
     reader.onloadend = () => {
       this.setState({ buffer: Buffer(reader.result) })
@@ -33,50 +33,52 @@ class AddLand extends Component {
     return (
       <div>
         <h1 className='title2'>Add Land</h1>
-        <form className='add-form' onSubmit={(event) => {
-          event.preventDefault()
-          const location = this.state.location
-          const value = window.web3.utils.toWei(this.state.value.toString(), 'Ether')
-          ipfs.add(this.state.buffer, async (error, result) => {
-            console.log('Ipfs result', result)
-            const image = result[0].hash
-            if(error) {
-              console.log(error)
-              return
-            }
-            await this.props.addLand(location, image, value)
-          })
-        }}>
-          <div className="form-group mr-sm-2">
-            <input
-              id="location"
-              type="text"
-              onChange={this.handleChange}
-              className="form-control"
-              placeholder="Location.."
-              required 
-            />
-            <input
-              id="value"
-              type="text"
-              onChange={this.handleChange}
-              className="form-control"
-              placeholder="Land value.."
-              required 
-            />
-            <div className='add-submit'>
-              <input 
-                type='file' 
-                onChange={this.captureFile} 
+        <div className='add-container'>
+          <form className='add-form' onSubmit={(event) => {
+            event.preventDefault()
+            const location = this.state.location
+            const value = window.web3.utils.toWei(this.state.value.toString(), 'Ether')
+            ipfs.add(this.state.buffer, async (error, result) => {
+              console.log('Ipfs result', result)
+              const image = result[0].hash
+              if(error) {
+                console.log(error)
+                return
+              }
+              await this.props.addLand(location, image, value)
+            })
+          }}>
+            <div className="form-group mr-sm-2">
+              <input
+                id="location"
+                type="text"
+                onChange={this.handleChange}
+                className="form-control"
+                placeholder="Location.."
+                required 
               />
-              <button 
-                type="submit" 
-                className="btn btn-primary">
-                Add Land
-              </button>
+              <input
+                id="value"
+                type="text"
+                onChange={this.handleChange}
+                className="form-control"
+                placeholder="Land value.."
+                required 
+              />
+              <div className='add-submit'>
+                <input 
+                  type='file' 
+                  onChange={this.captureFile} 
+                />
+                <button 
+                  type="submit" 
+                  className="btn btn-primary">
+                  Add Land
+                </button>
+              </div>
             </div>
-          </div>
-        </form>
+          </form>
+        </div>
       </div>
     )
   }
